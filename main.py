7c095f6bf859e803b84a368942a9b895bfa46952
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-12-12 01:32:21 -0500

    feat: add layer shell

--- main.py
+++ main.py
@@ -24,9 +24,16 @@ import calendar
 import asyncio
 import gi
 
-gi.require_version("Gtk", "4.0")
 
-from gi.repository import GLib, Gtk  # noqa: E402
+# For GTK4 Layer Shell to get linked before libwayland-client we must explicitly load it before importing with gi
+from ctypes import CDLL
+CDLL('libgtk4-layer-shell.so')
+
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Gtk4LayerShell", "1.0")
+
+from gi.repository import GLib, Gtk, Pango,  Gtk4LayerShell as GtkLayerShell  # noqa: E402
 
 
 TIME_PATH = os.path.expanduser("~/.time")
@@ -83,7 +90,7 @@ def get_default_styling() -> str:
 
 async def parse_agenda() -> list[str]:
     # FIXME: development fix
-    return "TODO: some tasks and stuf\n 1:20 - 2:20 more stuff".splitlines()
+    # return "TODO: some tasks and stuf\n 1:20 - 2:20 more stuff".splitlines()
     if not await is_running("emacs"):
         raise Exception("AgendaOffline")
 
@@ -138,17 +145,46 @@ def weatherBox() -> Gtk.Box:
     weather.append(weather_box)
     weather_box.append(Gtk.Label(label="Weather"))
     # FIXME: this is a temp fix, change on deploy
-    # weather_label = Gtk.Label(label=str(asyncio.run(get_weather())) + "°F")
-    weather_label = Gtk.Label(label="70°F")
+    weather_label = Gtk.Label(label=str(asyncio.run(get_weather())) + "°F")
+    # weather_label = Gtk.Label(label="70°F")
     weather_box.append(weather_label)
     apply_styles(weather_label, "label { font-size: 120px; }")
     return weather
 
+def Timer() -> Gtk.Box:
+    timer_box = VBox(10)
+
+    # replace with svg 
+    # ./alarm.svg
+    timer_image = Gtk.Label(label="⏰")
+    ibox = HBox()
+    ibox.append(timer_image)
+    ibox.append(Gtk.Entry())
+    timer_box.append(ibox)
+    timer_button = Gtk.Button(label="Start")
+    ibox.append(timer_button)
+
+    return timer_box
+
+def Time() -> Gtk.Box: 
+    """Returns time widget with time, and day"""
+    timeBox = VBox(20)
+
+    time_widget = Gtk.Label(label=dt.now().strftime("%I:%M %p"))
+    day_widget = Gtk.Label(label=dt.now().strftime("%A"))
+    # poll to refresh the time every minute
+    GLib.timeout_add_seconds(60, lambda: time_widget.set_label(dt.now().strftime("%I:%M %p")))
+    GLib.timeout_add_seconds(3600, lambda: day_widget.set_label(dt.now().strftime("%A")))
+    apply_styles(timeBox, "box {%s}" % get_default_styling())
+
+    timeBox.append(time_widget)
+    timeBox.append(day_widget)
+
+    return timeBox
 
 def Calendar() -> Gtk.Box:
     todays_date = dt.now()
     calendar_str = calendar.month(todays_date.year, todays_date.month)
-    print(calendar_str)
 
     calendar_box : Gtk.Box = Gtk.Box()
     calendar_label = Gtk.TextView()
@@ -156,12 +192,21 @@ def Calendar() -> Gtk.Box:
     calendar_label.set_cursor_visible(False)
     calendar_label.set_monospace(True)
     calendar_label.set_hexpand(True)
-    calendar_label.set_vexpand(True)
     apply_styles(calendar_label, "textview { font-size: %spx; }" % style.CALENDAR_FONT_SIZE) 
     apply_styles(calendar_box, "box {%s}" % get_default_styling())
     buffer = calendar_label.get_buffer()
     if buffer:
         buffer.set_text(calendar_str)
+
+        # Create a text tag to bolden today's date
+        tag: Gtk.TextTag = buffer.create_tag("bold", weight=Pango.Weight.ULTRABOLD)
+
+        # Find and bold the specific part of the text (today's date)
+        start_pos = buffer.get_iter_at_offset(calendar_str.find(str( todays_date.day )))
+        end_pos = buffer.get_iter_at_offset(calendar_str.find(str( todays_date.day )) + len(str(todays_date.day))
+) 
+        # Apply the bold tag to today's date
+        buffer.apply_tag(tag, start_pos, end_pos)
     else:
         raise Exception("Unable to create Calendar Buffer")
     calendar_box.append(calendar_label)
@@ -175,7 +220,7 @@ class Dashboard(Gtk.ApplicationWindow):
     def __init__(self, **kwargs):
         super().__init__(
             **kwargs,
-            title="dashbaord",
+            title="dashboard",
             show_menubar=False,
             child=None,
             fullscreened=False,
@@ -192,9 +237,18 @@ class Dashboard(Gtk.ApplicationWindow):
         # the other half shows the agenda
         self.set_child(self.main_box)
 
+        self.weatherBox = VBox(20)
         self.weather = weatherBox()
-        self.main_box.append(self.weather)
+        self.weatherBox.append(self.weather)
+        self.main_box.append(self.weatherBox)
 
+
+        self.calendarBox = VBox(20)
+        self.calendarBox.append(Time())
+        self.calendarBox.append(Calendar())
+        self.calendarBox.append(Timer())
+        self.main_box.append(self.calendarBox)
+        
         self.agenda_box = VBox(20)
         agenda_box: Gtk.Box = VBox(20)
         self.agenda_box.append(agenda_box)
@@ -203,11 +257,6 @@ class Dashboard(Gtk.ApplicationWindow):
             agenda_box,
             "box {%s}" % get_default_styling()
         )
-
-        self.calendar = Calendar()
-        self.main_box.append(self.calendar)
-        
-        # agenda title bar with the title
 
         self.agenda_title = Gtk.Box()
         apply_styles(self.agenda_title, "padding: 10px; background-color: #f0f0f0;")
@@ -221,8 +270,8 @@ class Dashboard(Gtk.ApplicationWindow):
             label = Gtk.Label(label=self.agenda[i])
             agenda_box.append(label)
 
-        self.timeBox = timeBox()
-        self.agenda_box.append(self.timeBox)
+        self.hourBox = timeBox()
+        self.agenda_box.append(self.hourBox)
 
     def on_button_clicked(self, _w: Gtk.Button):
         print("dashboard_exit")
@@ -230,9 +279,16 @@ class Dashboard(Gtk.ApplicationWindow):
 
 
 def on_activate(app: Gtk.Application):
-    # how to set the window to the layer above the desktop
-
+    # set to layer shell
     win = Dashboard(application=app)
+
+    GtkLayerShell.init_for_window(win)
+    GtkLayerShell.set_layer(win, GtkLayerShell.Layer.TOP)
+    GtkLayerShell.set_anchor(win, GtkLayerShell.Edge.BOTTOM, True)
+    GtkLayerShell.set_margin(win, GtkLayerShell.Edge.BOTTOM, 20)
+    GtkLayerShell.set_margin(win, GtkLayerShell.Edge.TOP, 20)
+    GtkLayerShell.auto_exclusive_zone_enable(win)
+
     win.present()
 
 
